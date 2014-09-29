ee2259d0fb5b2a26c90d6561d677e65771bcc811
Kimi SARAZU <kimisaraz@gmail.com>
2014-09-30 07:51:21 +0900

    Add '.emacs.d/inits/cocoa-emacs-config.el'

---
+++ .emacs.d/inits/cocoa-emacs-config.el
@@ -0,0 +1,12 @@
+;; commandキーをmetaとして使用する
+(setq ns-command-modifier (quote meta))
+;; optionキーをsuperとして使用する
+(setq ns-alternate-modifier (quote super))
+
+;; ファイルを開いたときに、新たなフレームを作らない
+(setq ns-pop-up-frames nil)
+
+;; ファイル名の設定
+(require 'ucs-normalize)
+(set-file-name-coding-system 'utf-8-hfs)
+(setq locale-coding-system 'utf-8-hfs)
