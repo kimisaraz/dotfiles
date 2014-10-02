cbf20e5c4269a5c631df2746e6316c7cfc62db31
Kimi SARAZU <kimisaraz@gmail.com>
2014-10-03 08:58:08 +0900

    Enable global-hl-line-mode and show-paren-mode

--- .emacs.d/inits/01_general.el
+++ .emacs.d/inits/01_general.el
@@ -14,6 +14,19 @@
 ;; インデントにタブ文字を使用しない
 (setq-default indent-tabs-mode nil)
 
+;;; 現在行のハイライト
+(global-hl-line-mode t)
+
+;;; 括弧の対応関係のハイライト
+;; 対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
+;; ;; parenのスタイル: expressionは括弧内も強調表示
+;; (setq show-paren-style 'expression)
+;; ;; フェイスを変更する
+;; (set-face-background  'show-paren-match-face nil)
+;; (set-face-underline-p  'show-paren-match-face "yellow")
+
 ;;; cua-modeの矩形編集を利用
 ;; cua-modeをオン
 (cua-mode t)
