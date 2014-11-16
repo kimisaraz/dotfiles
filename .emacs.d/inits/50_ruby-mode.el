69505e5d016fc405628b06d630ee12b131234a96
Kimi SARAZU <kimisaraz@gmail.com>
2014-11-17 00:51:59 +0900

    Add Ruby mode settings

---
+++ .emacs.d/inits/50_ruby-mode.el
@@ -0,0 +1,11 @@
+(setq ruby-deep-indent-paren-style nil)
+(setq ruby-insert-encoding-magic-comment nil)
+
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.builder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.jbuilder\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ruby\\'" . ruby-mode))
