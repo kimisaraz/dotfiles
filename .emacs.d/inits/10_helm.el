c708e17a900b0efdfe3484fc7f061447dd4b7287
Kimi SARAZU <kimisaraz@gmail.com>
2014-11-24 23:00:31 +0900

    Modify attributes of helm-related faces

--- .emacs.d/inits/10_helm.el
+++ .emacs.d/inits/10_helm.el
@@ -1,1 +1,13 @@
 (helm-mode 1)
+
+(set-face-attribute 'helm-source-header nil
+                    :height 1.0 :weight 'semi-bold :family nil
+                    :foreground "white" :background "gray5")
+(set-face-attribute 'helm-selection nil
+                    :background "gray10" :underline t)
+(set-face-attribute 'helm-visible-mark nil
+                    :background "NavyBlue")
+(set-face-attribute 'helm-ff-directory nil
+                    :foreground "cyan" :background nil :underline t)
+(set-face-attribute 'helm-buffer-directory nil
+                    :foreground "cyan" :background nil :underline t)
