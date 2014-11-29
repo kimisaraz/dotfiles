6101e3f88f1fa77b9b215fc1452eb6a64fa76d8b
Kimi SARAZU <kimisaraz@gmail.com>
2014-11-30 03:24:43 +0900

    Modify attributes of smartparens-related faces

--- .emacs.d/inits/10_smartparens.el
+++ .emacs.d/inits/10_smartparens.el
@@ -3,3 +3,8 @@
 (setq sp-show-pair-delay 0)
 (smartparens-global-mode t)
 (show-smartparens-global-mode t)
+
+(set-face-attribute 'sp-show-pair-match-face nil
+                    :background "#fd971f" :foreground "black")
+(set-face-attribute 'sp-show-pair-mismatch-face nil
+                    :background "#1e0010" :foreground "#960050")
