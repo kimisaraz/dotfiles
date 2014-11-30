38a3ff20a5a018bfcdd1bb0b571f2f77a0ba0929
Kimi SARAZU <kimisaraz@gmail.com>
2014-12-01 03:38:24 +0900

    Add smartparens local pair and tag definitions

--- .emacs.d/inits/10_smartparens.el
+++ .emacs.d/inits/10_smartparens.el
@@ -4,6 +4,15 @@
 (smartparens-global-mode t)
 (show-smartparens-global-mode t)
 
+(sp-with-modes '(org-mode markdown-mode gfm-mode text-mode)
+  (sp-local-pair "（" "）")
+  (sp-local-pair "「" "」")
+  (sp-local-pair "『" "』")
+  (sp-local-pair "【" "】"))
+
+(sp-with-modes '(markdown-mode gfm-mode)
+  (sp-local-tag "#" "# " " #"))
+
 (set-face-attribute 'sp-show-pair-match-face nil
                     :background "#fd971f" :foreground "black")
 (set-face-attribute 'sp-show-pair-mismatch-face nil
