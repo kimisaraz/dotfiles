4e90e39c32e301fead65d98009c9fa7ed98d9dee
Kimi SARAZU <kimisaraz@gmail.com>
2014-12-17 08:23:19 +0900

    Define my:direx-jump-to-project-root-other-window as a function

--- .emacs.d/inits/10_direx.el
+++ .emacs.d/inits/10_direx.el
@@ -1,1 +1,7 @@
 (require 'direx)
+
+(defun my:direx-jump-to-project-root-other-window ()
+  (interactive)
+  (or (ignore-errors
+        (direx-project:jump-to-project-root-other-window) t)
+      (direx:jump-to-directory-other-window)))
