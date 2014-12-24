fe7f1dd72ac345b99547fb7a9a7df244adfb3302
Kimi SARAZU <kimisaraz@gmail.com>
2014-12-25 00:05:02 +0900

    Show Calendar buffer in a popup window

--- .emacs.d/inits/10_popwin.el
+++ .emacs.d/inits/10_popwin.el
@@ -2,4 +2,5 @@
 (popwin-mode 1)
 
 (push '("^\\*helm.*\\*$" :regexp t :height 0.45) popwin:special-display-config)
+(push '(calendar-mode) popwin:special-display-config)
 (push '(direx:direx-mode :position left :width 36 :dedicated t) popwin:special-display-config)
