f33283d7ad1eac8ee0cb86d93b4d758e1ac377e0
Kimi SARAZU <kimisaraz@gmail.com>
2015-01-07 00:25:09 +0900

    Add a time stamp to the task when it moves to the DONE state

--- .emacs.d/inits/50_org-mode.el
+++ .emacs.d/inits/50_org-mode.el
@@ -1,3 +1,7 @@
 ;;; Basic
 ;; Default location to look for Org files
 (setq org-directory (concat my:dropbox-directory "org/"))
+
+;;; Tasks
+;; 完了状態になった時、CLOSEDタイムスタンプを付与する
+(setq org-log-done 'time)
