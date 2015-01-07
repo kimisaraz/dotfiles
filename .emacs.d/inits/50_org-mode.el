5a472634b400aee329442e156efd7e45e4b3f6bc
Kimi SARAZU <kimisaraz@gmail.com>
2015-01-08 00:37:07 +0900

    Open txt file with org-mode

--- .emacs.d/inits/50_org-mode.el
+++ .emacs.d/inits/50_org-mode.el
@@ -2,6 +2,8 @@
 ;; Default location to look for Org files
 (setq org-directory (concat my:dropbox-directory "org/"))
 
+(add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
+
 ;;; Tasks
 ;; 完了状態になった時、CLOSEDタイムスタンプを付与する
 (setq org-log-done 'time)
