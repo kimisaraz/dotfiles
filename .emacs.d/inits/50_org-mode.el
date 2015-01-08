13dd6fd7611b4dd9ab89e956058fd8d2d3f45a74
Kimi SARAZU <kimisaraz@gmail.com>
2015-01-09 07:55:39 +0900

    Change path for org-agenda-files

--- .emacs.d/inits/50_org-mode.el
+++ .emacs.d/inits/50_org-mode.el
@@ -4,6 +4,10 @@
 
 (add-to-list 'auto-mode-alist '("\\.txt\\'" . org-mode))
 
+;;; Agenda
+;; The files to be used for agenda display
+(setq org-agenda-files (list org-directory))
+
 ;;; Tasks
 ;; 完了状態になった時、CLOSEDタイムスタンプを付与する
 (setq org-log-done 'time)
