f17fe2d690619c35207de0a2db4c4c33fa72e761
Kimi SARAZU <kimisaraz@gmail.com>
2015-01-20 00:31:37 +0900

    Enable subword-mode when entering ruby-mode

--- .emacs.d/inits/50_ruby-mode.el
+++ .emacs.d/inits/50_ruby-mode.el
@@ -1,6 +1,11 @@
 (setq ruby-deep-indent-paren-style nil)
 (setq ruby-insert-encoding-magic-comment nil)
 
+(defun my:ruby-mode-hooks ()
+  "ruby-mode-hooks"
+  (subword-mode 1))
+(add-hook 'ruby-mode-hook 'my:ruby-mode-hooks)
+
 (add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.builder\\'" . ruby-mode))
