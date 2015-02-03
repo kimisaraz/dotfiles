535eecd4977ebad9feb413261061f7d68a3d1449
Kimi SARAZU <kimisaraz@gmail.com>
2015-02-04 08:22:13 +0900

    Refactor org-capture-templates

--- .emacs.d/inits/50_org-mode.el
+++ .emacs.d/inits/50_org-mode.el
@@ -11,10 +11,10 @@
 ;; Capture templates
 (setq org-capture-templates
       '(("t" "Tasks" entry
-         (file+headline (concat org-directory "agenda/tasks.org") "Inbox")
+         (file+headline nil "Inbox")
          "* TODO %?\n  %i")
         ("T" "Tasks (with timestamp)" entry
-         (file+headline (concat org-directory "agenda/tasks.org") "Inbox")
+         (file+headline nil "Inbox")
          "* TODO %?\n  %U\n  %i\n  %a")
         ("j" "Journal" entry
          (file+datetree (concat org-directory "journal.org"))
