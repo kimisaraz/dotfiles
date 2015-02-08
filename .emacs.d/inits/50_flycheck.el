a7ff735432492bbc3a6d76705a9e3befdf278874
Kimi SARAZU <kimisaraz@gmail.com>
2015-02-09 02:32:01 +0900

    Stop using global-flycheck-mode

--- .emacs.d/inits/50_flycheck.el
+++ .emacs.d/inits/50_flycheck.el
@@ -1,3 +1,4 @@
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
+
+(add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
+(add-hook 'ruby-mode-hook 'flycheck-mode)
