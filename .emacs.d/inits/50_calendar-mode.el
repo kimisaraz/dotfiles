bbbea2d55f181f2240483f243894e9f20178dbf4
Kimi SARAZU <kimisaraz@gmail.com>
2015-02-15 01:35:57 +0900

    Install japanese-holidays

--- .emacs.d/inits/50_calendar-mode.el
+++ .emacs.d/inits/50_calendar-mode.el
@@ -3,3 +3,9 @@
 
 ;; 今日をマークする
 (add-hook 'calendar-today-visible-hook 'calendar-mark-today)
+
+;; 祝日をカレンダーに表示
+(require 'japanese-holidays)
+(setq calendar-holidays
+      (append japanese-holidays holiday-local-holidays holiday-other-holidays))
+(setq mark-holidays-in-calendar t)
