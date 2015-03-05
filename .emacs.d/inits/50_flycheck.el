8aab6a607c501a8426e2dce2b96171f8146a1460
Kimi SARAZU <kimisaraz@gmail.com>
2015-03-06 08:03:12 +0900

    Enable flycheck on css-mode

--- .emacs.d/inits/50_flycheck.el
+++ .emacs.d/inits/50_flycheck.el
@@ -1,5 +1,6 @@
 (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))
 
+(add-hook 'css-mode-hook 'flycheck-mode)
 (add-hook 'emacs-lisp-mode-hook 'flycheck-mode)
 (add-hook 'js-mode-hook 'flycheck-mode)
 (add-hook 'ruby-mode-hook 'flycheck-mode)
