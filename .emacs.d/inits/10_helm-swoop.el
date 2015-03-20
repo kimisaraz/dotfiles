2b0b28f2c3a272973cf84479297788c4dedf209f
Kimi SARAZU <kimisaraz@gmail.com>
2015-03-20 23:49:32 +0900

    Modify helm-swoop setting

--- .emacs.d/inits/10_helm-swoop.el
+++ .emacs.d/inits/10_helm-swoop.el
@@ -1,1 +1,11 @@
 (require 'helm-swoop)
+
+;; (setq helm-multi-swoop-edit-save t)
+
+;; (setq helm-swoop-split-with-multiple-windows t)
+
+(setq helm-swoop-split-direction 'split-window-horizontally)
+
+;; (setq helm-swoop-speed-or-color t)
+
+(setq helm-swoop-move-to-line-cycle nil)
