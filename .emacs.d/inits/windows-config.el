ceea16f868bb2f54f09f584d8685341449135b45
Kimi SARAZU <kimisaraz@gmail.com>
2015-04-15 02:19:41 +0900

    Ignore <convert> and <non-convert> on windows

--- .emacs.d/inits/windows-config.el
+++ .emacs.d/inits/windows-config.el
@@ -1,3 +1,7 @@
 ;; ファイル名の設定
 (set-file-name-coding-system 'cp932)
 (setq locale-coding-system 'cp932)
+
+;; 変換/無変換を無視
+(global-set-key (kbd "<convert>")     'ignore)
+(global-set-key (kbd "<non-convert>") 'ignore)
