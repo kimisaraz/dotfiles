742deb198ee0d353798d3ebf2d035c279e3b091b
Kimi SARAZU <kimisaraz@gmail.com>
2015-04-21 00:05:58 +0900

    Run auto-complete-mode on coffee-mode

--- .emacs.d/inits/10_auto-complete.el
+++ .emacs.d/inits/10_auto-complete.el
@@ -4,6 +4,7 @@
 (setq ac-use-fuzzy t)
 ;; (add-to-list 'ac-dictionary-directories (expand-file-name "ac-dict" user-emacs-directory))
 
+(add-to-list 'ac-modes 'coffee-mode)
 (add-to-list 'ac-modes 'graphviz-dot-mode)
 (add-to-list 'ac-modes 'haml-mode)
 (add-to-list 'ac-modes 'scss-mode)
