0ac3c2c37d518138b8e617ccdcc78df9f25f920b
Kimi SARAZU <kimisaraz@gmail.com>
2015-04-28 23:31:02 +0900

    Apply monokai-theme

--- .emacs.d/inits/10_molokai-theme.el
+++ .emacs.d/inits/10_molokai-theme.el
@@ -1,14 +1,14 @@
-(require 'molokai-theme)
-(load-theme 'molokai t)
+;; (require 'molokai-theme)
+;; (load-theme 'molokai t)
 
-(custom-theme-set-faces
- 'molokai
- '(default ((t (:background "Black"))))
- '(region ((t (:background "NavyBlue" :inverse-video nil))))
- '(hl-line ((t (:background "gray10" :inverse-video nil))))
- '(font-lock-comment-face ((t (:foreground "gray50"))))
- '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
- '(isearch ((t (:foreground "Black" :background "Gray" :weight bold))))
- '(isearch-fail ((t (:foreground "Black" :background "OrangeRed"))))
- '(lazy-highlight ((t (:inherit isearch :weight normal))))
- '(trailing-whitespace ((t (:background "gray50")))))
+;; (custom-theme-set-faces
+;;  'molokai
+;;  '(default ((t (:background "Black"))))
+;;  '(region ((t (:background "NavyBlue" :inverse-video nil))))
+;;  '(hl-line ((t (:background "gray10" :inverse-video nil))))
+;;  '(font-lock-comment-face ((t (:foreground "gray50"))))
+;;  '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
+;;  '(isearch ((t (:foreground "Black" :background "Gray" :weight bold))))
+;;  '(isearch-fail ((t (:foreground "Black" :background "OrangeRed"))))
+;;  '(lazy-highlight ((t (:inherit isearch :weight normal))))
+;;  '(trailing-whitespace ((t (:background "gray50")))))
