78949ccabf5f2adec8b2a203222190a4ea1bce98
Kimi SARAZU <kimisaraz@gmail.com>
2015-04-29 23:39:24 +0900

    Modify monokai-theme

--- .emacs.d/inits/10_monokai-theme.el
+++ .emacs.d/inits/10_monokai-theme.el
@@ -1,2 +1,11 @@
 (require 'monokai-theme)
 (load-theme 'monokai t)
+
+(custom-theme-set-faces
+ 'monokai
+ '(default ((t (:background "Black"))))
+ '(region ((t (:background "NavyBlue"))))
+ '(hl-line ((t (:background "gray10"))))
+ '(fringe ((t :inherit hl-line)))
+ '(font-lock-comment-face ((t (:foreground "gray50"))))
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face)))))
