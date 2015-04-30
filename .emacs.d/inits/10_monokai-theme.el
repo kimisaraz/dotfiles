b95b7e0d58a587a010aefbc62491c89acbd343c0
Kimi SARAZU <kimisaraz@gmail.com>
2015-04-30 23:19:48 +0900

    Modify mode-line faces

--- .emacs.d/inits/10_monokai-theme.el
+++ .emacs.d/inits/10_monokai-theme.el
@@ -7,5 +7,7 @@
  '(region ((t (:background "NavyBlue"))))
  '(hl-line ((t (:background "gray10"))))
  '(fringe ((t :inherit hl-line)))
+ '(mode-line ((t (:inherit hl-line))))
+ '(mode-line-inactive ((t (:background "gray5" :foreground "gray20" :box "gray20"))))
  '(font-lock-comment-face ((t (:foreground "gray50"))))
  '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face)))))
