0fa3b237a43fc8a0dbbd670f5b827a40c5561eea
Kimi SARAZU <kimisaraz@gmail.com>
2015-05-01 11:45:32 +0900

    Modify helm faces

--- .emacs.d/inits/10_helm.el
+++ .emacs.d/inits/10_helm.el
@@ -11,14 +11,14 @@
 (add-to-list 'helm-boring-buffer-regexp-list "\\*Flycheck error messages\\*")
 (add-to-list 'helm-boring-buffer-regexp-list "\\*magit-process\\*")
 
-(set-face-attribute 'helm-source-header nil
-                    :height 1.0 :weight 'semi-bold :family nil
-                    :foreground "white" :background "gray5")
-(set-face-attribute 'helm-selection nil
-                    :background "gray10" :underline t)
-(set-face-attribute 'helm-visible-mark nil
-                    :background "NavyBlue")
-(set-face-attribute 'helm-ff-directory nil
-                    :foreground "cyan" :background nil :underline t)
-(set-face-attribute 'helm-buffer-directory nil
-                    :foreground "cyan" :background nil :underline t)
+;; (set-face-attribute 'helm-source-header nil
+;;                     :height 1.0 :weight 'semi-bold :family nil
+;;                     :foreground "white" :background "gray5")
+;; (set-face-attribute 'helm-selection nil
+;;                     :background "gray10" :underline t)
+;; (set-face-attribute 'helm-visible-mark nil
+;;                     :background "NavyBlue")
+;; (set-face-attribute 'helm-ff-directory nil
+;;                     :foreground "cyan" :background nil :underline t)
+;; (set-face-attribute 'helm-buffer-directory nil
+;;                     :foreground "cyan" :background nil :underline t)
--- .emacs.d/inits/10_monokai-theme.el
+++ .emacs.d/inits/10_monokai-theme.el
@@ -10,4 +10,12 @@
  '(mode-line ((t (:inherit hl-line))))
  '(mode-line-inactive ((t (:background "gray5" :foreground "gray20" :box "gray20"))))
  '(font-lock-comment-face ((t (:foreground "gray50"))))
- '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face)))))
+ '(font-lock-comment-delimiter-face ((t (:inherit font-lock-comment-face))))
+ ;; helm
+ '(helm-source-header ((t (:background "gray5" :foreground "white" :height 1.0 :wiight 'semi-bold :family nil))))
+ '(helm-selection ((t (:background "gray10" :underline t))))
+ '(helm-visible-mark ((t (:background "NavyBlue"))))
+ '(helm-ff-file ((t (:background nil))))
+ '(helm-ff-directory ((t (:background nil :foreground "#66D9EF"))))
+ '(helm-ff-dotted-directory ((t (:inherit helm-ff-directory :foreground "#21889B"))))
+ '(helm-buffer-directory ((t (:inherit helm-ff-directory)))))
