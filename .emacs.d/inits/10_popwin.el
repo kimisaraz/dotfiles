772a84ce45f9ea577029c6f9ab2b02e6cea63419
Kimi SARAZU <kimisaraz@gmail.com>
2016-02-01 23:58:23 +0900

    Show helm buffer without popwin

--- .emacs.d/inits/10_popwin.el
+++ .emacs.d/inits/10_popwin.el
@@ -1,6 +1,6 @@
 (require 'popwin)
 (popwin-mode 1)
 
-(push '("^\\*helm.*\\*$" :regexp t :height 0.45) popwin:special-display-config)
+;; (push '("^\\*helm.*\\*$" :regexp t :height 0.45) popwin:special-display-config)
 (push '(calendar-mode) popwin:special-display-config)
 (push '(direx:direx-mode :position left :width 36 :dedicated t) popwin:special-display-config)
